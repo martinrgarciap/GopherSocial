846efa3506d706da9a99ef8117f36ae55bf78410
Martin Garcia <martinrgarciap@gmail.com>
2026-04-18 22:46:08 -0400

    Fix getuserfeed function to use user from context

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -44,8 +44,9 @@ func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Reques
 	}
 
 	ctx := r.Context()
+	user := getUserFromContext(r)
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(341), fq)
+	feed, err := app.store.Posts.GetUserFeed(ctx, user.ID, fq)
 	if err != nil {
 		app.internalServerResponse(w, r, err)
 		return
